28705d7e0b42c8a70a806e79bfe41f36efb7551a
JustinTafua <j.tafua1990@gmail.com>
2025-10-01 21:00:38 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers(){
+    return [{ source: '/:path*', headers: [{ key: 'Permissions-Policy', value: 'camera=(self)' }] }];
+  }
+};
+module.exports = nextConfig;
