902d7780593efa00316aee3165a65a5606b94e96
JustinTafua <j.tafua1990@gmail.com>
2025-10-03 23:55:22 -0500

    Update PageShell.jsx

--- src/components/PageShell.jsx
+++ src/components/PageShell.jsx
@@ -1,11 +1,16 @@
-// src/components/PageShell.jsx
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import GarageBackground from "./GarageBackground";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import GarageBackground from "../components/GarageBackground";
+import { useRouter } from "next/router";
 
 export default function PageShell({ children }) {
+  const { pathname } = useRouter();
+
+  // Option: hide animated props on camera page to reduce distractions
+  const showProps = pathname !== "/scan";
+
   return (
-    <GarageBackground>
+    <GarageBackground showProps={showProps}>
       <Navbar />
       <main style={{ padding: "16px 16px 80px" }}>{children}</main>
       <Footer />
