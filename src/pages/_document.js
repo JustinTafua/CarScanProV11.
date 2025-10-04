efff5daa4107a8aaf37b201e3115191aa8d95f08
JustinTafua <j.tafua1990@gmail.com>
2025-10-04 00:27:43 -0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,14 @@
-// src/pages/_document.js
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* PWA basics */}
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#111111" />
-        <link rel="icon" href="/icons/icon-192.png" sizes="192x192" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        {/* ✅ Preload garage and Skyline mockups from /public/icons */}
+        <link rel="preload" as="image" href="/icons/mockup_stage1.png" />
+        <link rel="preload" as="image" href="/icons/mockup_stage2.png" />
+        <link rel="preload" as="image" href="/icons/mockup_stage3.png" />
+        <link rel="preload" as="image" href="/icons/mockup_stage4.png" />
       </Head>
       <body>
         <Main />
