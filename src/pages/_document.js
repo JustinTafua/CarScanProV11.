d8211dfb9cee11e8328829f190c3368b30acf064
JustinTafua <j.tafua1990@gmail.com>
2025-10-04 00:09:28 -0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,12 +10,6 @@ export default function Document() {
         <meta name="theme-color" content="#111111" />
         <link rel="icon" href="/icons/icon-192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
-
-        {/* Preload build-stage backgrounds to avoid flicker */}
-        <link rel="preload" as="image" href="/mockup_stage1.png" />
-        <link rel="preload" as="image" href="/mockup_stage2.png" />
-        <link rel="preload" as="image" href="/mockup_stage3.png" />
-        <link rel="preload" as="image" href="/mockup_stage4.png" />
       </Head>
       <body>
         <Main />
@@ -23,4 +17,4 @@ export default function Document() {
       </body>
     </Html>
   );
-} 
+}
